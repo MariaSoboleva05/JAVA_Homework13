724b16b465ef30921205f9b11add81db621ffbc3
Maria_Soboleva <sobolewamary@yandex.ru>
2022-07-28 21:17:53 +0300

    Modified after remarks

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -8,8 +8,10 @@ public class ProductManagerTest {
 
     Product product1 = new Book(1, "Евгений Онегин", 300, "А.С.Пушкин");
     Product product2 = new Book(222, "Преступление и наказание", 350, "Ф.М. Достоевский");
-    Product product3 = new Smartphone(15, "Iphone 12", 50_000, "Apple");
-    Product product4 = new Smartphone(333, "Xiaomi Redmi 10", 27_000, "Xiaomi");
+    Product product3 = new Book(42, "Алхимик", 350, "П.Коэльо");
+    Product product4 = new Book(58, "Алхимик", 350, "Г.Ф. Лавкрафт");
+    Product product5 = new Smartphone(15, "Iphone 12", 50_000, "Apple");
+    Product product6 = new Smartphone(333, "Xiaomi Redmi 10", 27_000, "Xiaomi");
 
 
     @Test
@@ -18,6 +20,8 @@ public class ProductManagerTest {
         manager.add(product2);
         manager.add(product3);
         manager.add(product4);
+        manager.add(product5);
+        manager.add(product6);
 
         Product[] expected = {product1};
         Product[] actual = manager.searchBy("Евгений Онегин");
@@ -31,19 +35,23 @@ public class ProductManagerTest {
         manager.add(product2);
         manager.add(product3);
         manager.add(product4);
+        manager.add(product5);
+        manager.add(product6);
 
-        Product[] expected = {product4};
+        Product[] expected = {product6};
         Product[] actual = manager.searchBy("Xiaomi Redmi 10");
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotSearchByWrongName() {
+    public void shouldNotSearchByNameThatDoesNotExist() {
         manager.add(product1);
         manager.add(product2);
         manager.add(product3);
         manager.add(product4);
+        manager.add(product5);
+        manager.add(product6);
 
         Product[] expected = {};
         Product[] actual = manager.searchBy("Анна Каренина");
@@ -62,10 +70,25 @@ public class ProductManagerTest {
 
     @Test
     public void shouldSearchIfOneProductAdded() {
-        manager.add(product4);
+        manager.add(product2);
 
-        Product[] expected = {product4};
-        Product[] actual = manager.searchBy("Xiaomi Redmi 10");
+        Product[] expected = {product2};
+        Product[] actual = manager.searchBy("Преступление и наказание");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchMoreThanOneProduct() {
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+        manager.add(product4);
+        manager.add(product5);
+        manager.add(product6);
+
+        Product[] expected = {product3, product4};
+        Product[] actual = manager.searchBy("Алхимик");
 
         Assertions.assertArrayEquals(expected, actual);
     }
